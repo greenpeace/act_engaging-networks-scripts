4752c8fbdacbc1075fd595ff11e7c3e5609cef0c
Eoin Dubsky <edubsky@greenpeace.org>
2014-03-27 14:06:03 +0100

    added inline README
    1. In Silverpop: Create a query containing only the people who've participated in, say, TSW (e.g. "WHERE 'TSW' is not NULL").
    2. In Silverpop: Export the query, with only the field "email".
    3. In (a php script I'm writing now): CSV export file from Silverpop is split every 20,000 lines, with ",Y,P" added to the end of each line too.
    4. In Engaging Networks: A campaign is created - in the case of TSW for the Brazil account it will be called "pt.forests.tigers.signup.manifesto".
    5. In Engaging Networks: The CSV files created in step 3 are imported using the Supporter data > Import Supporters wizard. The first time it is done we create a saved format like "Tiger Shop Window Manifesto (Silverpop)", then we use that format to speed up the other file imports too. Each time we're just mapping email=email, Y=email_ok and P=pt.forests.tigers.signup.manifesto.

--- en-manual-upload-helper.php
+++ en-manual-upload-helper.php
@@ -11,6 +11,23 @@
  * php en-manual-upload-helper.php example.csv
  *
  * Author: Eoin Dubsky <edubsky@greenpeace.org>
+ * 
+ * More details:
+ * 1. In Silverpop: Create a query containing only the people who've participated in, say, 
+ * TSW (e.g. "WHERE 'TSW' is not NULL").
+ * 2. In Silverpop: Export the query, with only the field "email".
+ * 3. In this script: CSV export file from Silverpop is split every 20,000 
+ * lines, with ",Y,P" added to the end of each line too.
+ * 4. In Engaging Networks: A campaign is created - in the case of TSW for the Brazil account 
+ * it will be called "pt.forests.tigers.signup.manifesto".
+ * 5. In Engaging Networks: The CSV files created in step 3 are imported using the 
+ * Supporter data > Import Supporters wizard. The first time it is done we create a saved format 
+ * like "Tiger Shop Window Manifesto (Silverpop)", then we use that format to speed up the other 
+ * file imports too. Each time we're just mapping email=email, Y=email_ok 
+ * and P=pt.forests.tigers.signup.manifesto.
+ * 
+ * 
+ * 
  */
 
 
