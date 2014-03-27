bc7eea8978e644bac0ab97bbbaddd020fb1b5a30
Eoin Dubsky <edubsky@greenpeace.org>
2014-03-28 00:36:27 +0100

    Added $val = preg_replace('/[^,]""[^,]/','',$val);

--- en-manual-upload-helper.php
+++ en-manual-upload-helper.php
@@ -11,23 +11,6 @@
  * php en-manual-upload-helper.php example.csv
  *
  * Author: Eoin Dubsky <edubsky@greenpeace.org>
- * 
- * More details:
- * 1. In Silverpop: Create a query containing only the people who've participated in, say, 
- * TSW (e.g. "WHERE 'TSW' is not NULL").
- * 2. In Silverpop: Export the query, with only the field "email".
- * 3. In this script: CSV export file from Silverpop is split every 20,000 
- * lines, with ",Y,P" added to the end of each line too.
- * 4. In Engaging Networks: A campaign is created - in the case of TSW for the Brazil account 
- * it will be called "pt.forests.tigers.signup.manifesto".
- * 5. In Engaging Networks: The CSV files created in step 3 are imported using the 
- * Supporter data > Import Supporters wizard. The first time it is done we create a saved format 
- * like "Tiger Shop Window Manifesto (Silverpop)", then we use that format to speed up the other 
- * file imports too. Each time we're just mapping email=email, Y=email_ok 
- * and P=pt.forests.tigers.signup.manifesto.
- * 
- * 
- * 
  */
 
 
@@ -40,12 +23,14 @@ $counter = 1;
 
 // split into files called upload_part_#.csv every 20,000 lines
 while ($chunk = array_splice($in, 0, 20000)){
-      $f = fopen("upload_part_".($counter++).".csv", "a");
+      $f = @fopen("upload_part_".($counter++).".csv", "a");
 	  fwrite($f, $header);
       fwrite($f, implode("", $chunk));
       fclose($f);
 }
 
+// go back one, so we don't make too many files
+$counter=$counter-1; 
 
 // add the Y and P columns
 while ($counter > 0) {
@@ -56,7 +41,9 @@ while ($counter > 0) {
 	$fp = fopen($output,"w");
 
 	foreach((array)$data as $val) {
-
+	   // first remove stray "double quotes"
+	   $val = preg_replace('/[^,]""[^,]/','',$val);
+	   // then add our Y and P columns 
 	   fwrite($fp,$val.",Y,P\r\n");   
 	}
 	fclose($fp);
